a05fc0e6747c062771de76ad0d480c4b14219f77
danielramirez <danielramirez@192.168.0.3>
2016-12-08 23:49:38 -0600

    final version week2-hw2

--- Algorithms_onGraphs_Week2_HW2/src/Algorithms_onGraphs_Week2_HW2.cpp
+++ Algorithms_onGraphs_Week2_HW2/src/Algorithms_onGraphs_Week2_HW2.cpp
@@ -11,31 +11,77 @@
 
 using namespace std;
 using std::vector;
-using std::pair;
 
-void dfs(vector<vector<int> > &adj, vector<int> &used, vector<int> &order, int x) {
-  //write your code here
+//prototypes:
+void explore(vector<vector<int> > &adj, vector<bool> &adj_v, int x);
+void acyclic(vector<vector<int> > &adj);
+void processed(vector<int> &v_processing);
+void print_order(vector<int> &order_courses);
+
+//global variables
+size_t n, m,num_cycle=0;
+bool cycle,sink;
+vector<int> order_courses;
+
+void processed(vector<int> &vector_set){
+	vector<int>::iterator it;
+	for(it=vector_set.begin();it!=vector_set.end();++it)
+		cout<<" *it="<<*it<<endl;
 }
 
-vector<int> toposort(vector<vector<int> > adj) {
-  vector<int> used(adj.size(), 0);
-  vector<int> order;
-  //write your code here
-  return order;
+void print_order(vector<int> &order_courses){
+     while(!order_courses.empty()){
+        cout<<order_courses.back()+1<<" ";
+        order_courses.pop_back();
+     }
+}
+
+void explore(vector<vector<int> > &adj, vector<int> &v_processing, vector<int> &v_visited,int x){
+	vector<int>::iterator it;
+	v_processing[x]=x;
+	sink=(adj[x].empty()) ? true:false;
+	if(sink && v_visited[x]!=x){
+	     v_visited[x]=x;
+	     v_processing[x]=-1;
+	     order_courses.push_back(x);
+	}
+	else{
+	  for(it=adj[x].begin();it!=adj[x].end();it++){
+	     cycle=(v_processing[*it]==*it) ? true:false;
+         if(!cycle && v_visited[*it]!=*it)
+	  	      explore(adj,v_processing,v_visited,*it);
+         else if (cycle) num_cycle++;
+	     v_processing[x]=-1;
+	  	 v_visited[x]=x;
+	  }
+	  if(v_visited[x]==x)
+	  	 order_courses.push_back(x);
+   }
+}
+
+void acyclic(vector<vector<int> > &adj) {
+	vector<int> v_visited(n,-1);
+	vector<int> v_processing(n,-1);
+    for(int i=0;i<n;i++){
+	   if(v_visited[i]!=i)
+		   explore(adj,v_processing, v_visited, i);
+	}
+    if(num_cycle==0)
+        print_order(order_courses);
 }
 
 int main() {
-	  size_t n, m;
-	  std::cin >> n >> m;
-	  vector<vector<int> > adj(n, vector<int>());
-	  for (size_t i = 0; i < m; i++) {
-	    int x, y;
-	    std::cin >> x >> y;
-	    adj[x - 1].push_back(y - 1);
+	  cin >> n >> m;
+	  if( (n>=1 && n<=1e5) && (m>=0 && m<=1e5) ){
+		  vector<vector<int> > adj(n, vector<int>());
+		  for (size_t i = 0; i < m; i++) {
+			  int x, y;
+			  cin >> x >> y;
+			  adj[x - 1].push_back(y - 1);
+		  }
+		  acyclic(adj);
 	  }
-	  vector<int> order = toposort(adj);
-	  for (size_t i = 0; i < order.size(); i++) {
-	    std::cout << order[i] + 1 << " ";
-	  }
-	  return 0;
+	  else
+		  cout << 0;
+	return 0;
 }
